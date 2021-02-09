1235759f1fefc270d1af77333f74036475a25170
joão <joaomarcos70outlook.com>
2021-02-09 18:17:27 -0300

    implementando rota atendimento
    implementando GET e POST na rota

---
+++ controllers/atendimentos.js
@@ -0,0 +1,9 @@
+module.exports = (app) => {
+  app.get("/atendimentos", (req, res) =>
+    res.send("voce está na rota de atendimentos e esta realizando um GET")
+  );
+
+  app.post("/atendimentos", (req, res) =>
+    res.send("voce está na tora de atendimentos e esta realizando um POST")
+  );
+};
