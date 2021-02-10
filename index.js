42ea6dc6b1d828596f2f5a13a71ba7b91398f660
joão <joaomarcos70outlook.com>
2021-02-10 14:25:54 -0300

    conectando ao banco
    resolvendo problema de conexão com o node

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const customExpress = require('./config/customExpress')
+const customExpress = require("./config/customExpress");
+const conexao = require("./infraestrutura/conexao");
+const app = customExpress();
 
-const app = customExpress()
-
-app.listen(3000, () => console.log("servidor rodando na porta 3000"));
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    console.log("conectado com sucesso");
+    app.listen(3000, () => console.log("servidor rodando na porta 3000"));
+  }
+});
