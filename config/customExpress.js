807af86f216535b1a56d4c9d85d8b4febf2e74f1
joão <joaomarcos70outlook.com>
2021-02-10 10:00:37 -0300

    configurando a api para receber req
    aceitando req. urlencoded
    aceitando json

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const consign = require("consign");
+const bodyParser = require("body-parser");
 
 module.exports = () => {
   const app = express();
 
-  consign()
-    .include('controllers')
-    .into(app);
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  consign().include("controllers").into(app);
 
-  return app
+  return app;
 };
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,7 +3,9 @@ module.exports = (app) => {
     res.send("voce está na rota de atendimentos e esta realizando um GET")
   );
 
-  app.post("/atendimentos", (req, res) =>
+  app.post("/atendimentos", (req, res) => {
+    console.log(req.body)
     res.send("voce está na tora de atendimentos e esta realizando um POST")
-  );
+  });
+  
 };
