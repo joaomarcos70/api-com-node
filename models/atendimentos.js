9d3151d4d7351033fb65f37b602e4827e1905fd9
joão <joaomarcos70outlook.com>
2021-02-10 17:50:53 -0300

    fazendo tratativa de erros e validando regra de negocio

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -6,6 +6,30 @@ class Atendimento{
     adiciona(atendimento, res){
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+            nome:'data',
+            valido: dataEhValida,
+            mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+            nome:'cliente',
+            valido: clienteEhValido,
+            mensagem: 'Cliente deve conter pelo menos 5 caracteres'
+            },
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros){
+            res.status(400).json(erros)
+        }else{
+
         const atendimentoDatado = {...atendimento, dataCriacao, data}
         const sql = 'INSERT INTO atendimentos SET ?'
 
@@ -16,6 +40,9 @@ class Atendimento{
                 res.status(201).json(resultados)
             }
         })
+        
+        }
+
     }
 }
 
