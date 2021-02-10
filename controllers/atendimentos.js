acd9c80ff1c46be1f6eb4e58f926e48f5308f22d
joão <joaomarcos70outlook.com>
2021-02-10 18:58:04 -0300

    implementando o list
    lista com filtro
    e lista geral

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,16 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = (app) => {
-  app.get("/atendimentos", (req, res) =>
-    res.send("voce está na rota de atendimentos e esta realizando um GET")
-  );
+  app.get("/atendimentos", (req, res) =>{
+    Atendimento.lista(res)
+  });
+
+  app.get('/atendimentos/:id',(req, res)=>{
+  const id = parseInt(req.params.id)
+
+  Atendimento.buscaPorId(id, res)
+
+  })
 
   app.post("/atendimentos", (req, res) => {
     const atendimento = req.body
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -40,9 +40,34 @@ class Atendimento{
                 res.status(201).json(resultados)
             }
         })
-        
+
         }
 
+    }
+
+    lista(res){
+        const sql = 'SELECT * FROM atendimentos'
+
+        conexao.query(sql,(erro, resultados) => {
+            if(erro){
+                res.resultados(400).json(erro)
+            }else{
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    buscaPorId(id, res){
+        const sql = `SELECT * FROM atendimentos WHERE id =${id}`
+
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados[0]
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json(atendimento)
+            }
+        })
     }
 }
 
