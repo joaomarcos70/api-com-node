8a4d1b313153a7f66742e989ba7a16a5f1bd06c3
joão <joaomarcos70outlook.com>
2021-02-10 15:13:19 -0300

    tabela do banco criada

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const customExpress = require("./config/customExpress");
 const conexao = require("./infraestrutura/conexao");
+const Tabelas = require('./infraestrutura/tabelas');
 const app = customExpress();
 
 conexao.connect((erro) => {
@@ -7,6 +8,7 @@ conexao.connect((erro) => {
     console.log(erro);
   } else {
     console.log("conectado com sucesso");
+    Tabelas.init(conexao)
     app.listen(3000, () => console.log("servidor rodando na porta 3000"));
   }
 });
