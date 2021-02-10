9b40939112e43811de6029ae244256d61ca53d4c
joão <joaomarcos70outlook.com>
2021-02-10 15:44:51 -0300

    cadastrando no banco

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,14 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = (app) => {
   app.get("/atendimentos", (req, res) =>
     res.send("voce está na rota de atendimentos e esta realizando um GET")
   );
 
   app.post("/atendimentos", (req, res) => {
-    console.log(req.body)
+    const atendimento = req.body
+
+    Atendimento.adiciona(atendimento)
     res.send("voce está na tora de atendimentos e esta realizando um POST")
   });
   
