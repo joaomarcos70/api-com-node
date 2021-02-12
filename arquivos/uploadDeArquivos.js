723e570f11fad8a2888bae6c1e934c81fd15f604
joão <joaomarcos70outlook.com>
2021-02-12 10:35:22 -0300

    configurando o tipo de imagens a ser recebido
    usando path

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,8 +1,20 @@
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path')
+
+
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
 
-   const novoCaminho = `./assets/imagens/${nomeDoArquivo}` 
+  const tiposValidos = ['jpg', 'png', 'jpeg']  
+  const tipo = path.extname(caminho)
+  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+
+  if(tipoEhValido === -1){
+    console.log('tipo invalido')
+  }else{
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+  }
+  const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}` 
     
   fs.createReadStream(caminho)
     .pipe(fs.createWriteStream(novoCaminho))
