4a9de0ec9cb8f0e5aa4517e36366003121305d3a
joão <joaomarcos70outlook.com>
2021-02-12 13:57:17 -0300

    consumindo api de cpf

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,23 +1,20 @@
-const fs = require('fs');
-const path = require('path')
-
-
+const fs = require("fs");
+const path = require("path");
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+  const tiposValidos = ["jpg", "png", "jpeg"];
+  const tipo = path.extname(caminho);
+  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1;
 
-  const tiposValidos = ['jpg', 'png', 'jpeg']  
-  const tipo = path.extname(caminho)
-  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+  if (tipoEhValido) {
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`;
 
-  if(tipoEhValido === -1){
-    console.log('tipo invalido')
-  }else{
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+    fs.createReadStream(caminho)
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on("finish", () => callbackImagemCriada(false, novoCaminho));
+  } else {
+    const erro = "tipo é invalido";
+    console.log("tipo invalido");
+    callbackImagemCriada(erro);
   }
-  const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}` 
-    
-  fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho))
-  
 };
--- models/pets.js
+++ models/pets.js
@@ -5,16 +5,20 @@ class Pet {
   adiciona(pet, res) {
     const query = "INSERT INTO pets SET ?";
 
-    uploadDeArquivo(pet.imagem, pet.nome, (novoCaminho) => {
-        const novoPet ={nome: pet.nome, imagem: novoCaminho}
-      conexao.query(query, pet, (erro) => {
-        if (erro) {
-          console.log(erro);
-          res.status(400).json(erro);
-        } else {
-          res.status(200).json(novoPet);
-        }
-      });
+    uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+      if (erro) {
+          res.status(400).json(erro)
+      } else {
+        const novoPet = { nome: pet.nome, imagem: novoCaminho };
+        conexao.query(query, pet, (erro) => {
+          if (erro) {
+            console.log(erro);
+            res.status(400).json(erro);
+          } else {
+            res.status(200).json(novoPet);
+          }
+        });
+      }
     });
   }
 }
